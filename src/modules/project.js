460a4ab099c561d6353a6ce6f6b3aa0f892404b0
MacJ13 <jacdim02@gmail.com>
2023-03-28 20:42:51 +0200

    Create 'project' function factory

---
+++ src/modules/project.js
@@ -0,0 +1,58 @@
+import Task from "./task";
+
+export default function Project(projectTitle, projectId) {
+  let id = projectId || Date.now() + "proj";
+  let title = projectTitle;
+  let tasks = [];
+  let currentTask;
+
+  function getTasks() {
+    return tasks;
+  }
+
+  function findTask(id) {
+    return tasks.find((task) => task.getId() === id);
+  }
+
+  function getCurrentTask() {
+    return currentTask;
+  }
+
+  function setCurrentTask(id) {
+    currentTask = tasks.find((task) => id === task.getId());
+  }
+
+  function getId() {
+    return id;
+  }
+
+  function getTitle() {
+    return title;
+  }
+
+  function setTitle(newTitle) {
+    title = newTitle;
+  }
+
+  function addTask({ title, description, priority, duedate }) {
+    const task = new Task(title, description, duedate, priority);
+    tasks.push(newTask);
+    currentTask = task;
+  }
+
+  function removeTask(id) {
+    tasks = tasks.filter((task) => task.id !== id);
+  }
+
+  return {
+    getTasks,
+    getId,
+    getTitle,
+    setTitle,
+    addTask,
+    removeTask,
+    getCurrentTask,
+    setCurrentTask,
+    findTask,
+  };
+}
